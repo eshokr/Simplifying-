197159273ebba9905e047c4099019313384e68e8
eshokr <43872520+eshokr@users.noreply.github.com>
2022-11-24 16:18:52 +0100

    Add files via upload
    This code is to generate histograms with different names; instead of defining all the histograms; you can use this code to save your time !!

---
+++ GeneratingHistogramsWithDifferentNames.cpp
@@ -0,0 +1,26 @@
+
+{
+ TF1 *f1 = new TF1("f1","abs(sin(x)/x)*sqrt(x)",0,20);
+TH1F *myhist[4];
+
+ const char* histname[4]  = { "histo_1", "histo_2", "histo_3", "histo_4" };
+ 
+ for (int i = 0; i < 4; i++)
+ std::cout << histname[i] << "\n";
+
+int nbins=20,nfiles=5;
+float xmin=0,xmax=20;
+for (int i=0;i<4;i++) {
+//sprintf(histname,"histo%d",i);
+ myhist[i] = new TH1F(histname[i],"",nbins, xmin, xmax);
+ 
+ for (int j=0;j<40;j++) {
+  double r = f1->GetRandom(); 
+   myhist[i]->Fill(r);
+ }   
+}
+
+myhist[3] ->Draw("SAME");
+  myhist[2] ->Draw("SAME");
+  myhist[1] ->Draw("SAME");
+}
